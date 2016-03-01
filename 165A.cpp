e8cc6111192201d6dd731f370f523557344f54b1
Jaskamal Kainth <kainthjaskamal@gmail.com>
2016-03-01 11:03:08 +0530

    geometry , interior angel sum regular polygon

---
+++ 165A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t; cin >> t;
+	while(t--)
+	{
+		int x; cin >> x;
+		if( 360%(180-x) == 0 )
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+	}
+
+	return 0;
+}
