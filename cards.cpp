b37eea2b69e3ff5cd1204b93ec5c32ed0428c7de
jaskamalkainth <kainthjaskamal@gmail.com>
2016-07-29 14:09:04 +0530

    Adhoc , greedy , Logic

---
+++ cards.cpp
@@ -0,0 +1,100 @@
+/*
+ *    J1K7_7
+ */
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <set>
+#include <cstring>
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <limits>
+using namespace std;
+typedef long long          ll;
+typedef unsigned long long ull;
+typedef long double        ld;
+typedef pair<int,int>      pii;
+typedef pair<ll,ll>        pll;
+typedef vector<int>        vi;
+typedef vector<long long>  vll;
+#define left(x) 		   (x << 1)
+#define right(x) 		   (x << 1) + 1
+#define mid(l, r) 	       ((l + r) >> 1)
+#define mp                 make_pair
+#define pb                 push_back
+#define all(a)             a.begin(),a.end()
+#define debug(x)	       {cerr <<#x<<" = " <<x<<"\n"; }
+#define debug2(x, y)       {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<"\n";}
+#define debug3(x, y, z)    {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<", "<<#z<<" = "<<z<<"\n";}
+#define debug4(x, y, z, w) {cerr <<#x<<" = " <<x<<", "<<#y <<" = " <<y <<", "<<#z<<" = "<<z<<", "<<#w<<"\n";}
+#define ss                 second
+#define ff                 first
+#define m0(x) 		       memset(x,0,sizeof(x))
+
+const int mod=1e9+7;
+
+template<class T> inline T gcd(T a,T b){ll t;while(b){a=a%b;t=a;a=b;b=t;}return a;}
+template<class T> inline T lcm(T a,T b){return a/gcd(a,b)*b;}
+template<typename T, typename S> T expo(T b, S e, const T &m){if(e <= 1)return e == 0?1: b;\
+	return (e&1) == 0?expo((b*b)%m, e>>1, m): (b*expo((b*b)%m, e>>1, m))%m;}
+template<typename T> T modinv(T a) { return expo(a, mod-2, mod); }
+
+inline int nextint(){ int x; scanf("%d",&x); return x; }
+inline ll nextll(){ ll x; scanf("%lld",&x); return x; }
+
+const ll  mx_ll   = numeric_limits<ll> :: max();
+const int mx_int  = numeric_limits<int> :: max();
+
+const long double PI = (long double)(3.1415926535897932384626433832795);
+
+bool ok[203]={0};
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+
+	int n; scanf("%d",&n);
+	vi a(n);
+	int sum = 0;
+	for(int i = 0;i < n; i++)
+	{
+		scanf("%d",&a[i]);
+		sum += a[i];
+	}
+	sum = sum/(n/2);
+	vector<pii> ans;
+	for(int i = 0; i < n; i++)
+	{
+		if(ok[i]) continue;
+		ok[i] = 1;
+		for(int j = i+1;j < n; j++)
+		{
+			if((!ok[j]) && ((a[i]+a[j]) == sum))
+			{
+				ans.pb(mp(i+1,j+1));
+				ok[j] = 1;
+				break;
+			}
+		}
+	}
+	for(auto x: ans)
+	{
+		printf("%d %d\n",x.ff,x.ss);  
+	}
+    return 0;
+}
+
